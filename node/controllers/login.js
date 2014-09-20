e53ea3d8c9d4d173e11fa72ba7d95e785a98163d
Emilio <emendoza2012@yahoo.com>
2014-09-20 01:41:09 -0400

    node callse login

--- node/controllers/login.js
+++ node/controllers/login.js
@@ -1,10 +1,17 @@
 module.exports = function(app, modules) {
     var section = '/login/'
 
+	
     // Controller
     app.get(section + 'login', function(req, res) {
         res.render('login/login.html');
     });
-
+	
+	
     // Api Controller
+	
+	app.get('/actions/login/login', function(req, res) {
+		var response = req.query.response;
+		console.log(response);
+	});
 }
