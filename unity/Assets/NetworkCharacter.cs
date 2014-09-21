9dabf0b3ee5bd677c7c0eb1805c61d7fdbee6b7c
Paul Park <camo1018@yahoo.com>
2014-09-21 04:39:32 -0400

    Networking id and name

--- unity/Assets/NetworkCharacter.cs
+++ unity/Assets/NetworkCharacter.cs
@@ -6,6 +6,9 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 	Vector3 realPosition;
 	Quaternion realRotation;
 	
+	string realId;
+	string realName;
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -20,6 +23,8 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 			transform.position = Vector3.Lerp(transform.position, realPosition, 0.1f);
 			transform.Find("ForwardDirection").rotation = Quaternion.Lerp(transform.Find("ForwardDirection").rotation, realRotation, 0.1f);
 			transform.Find("OVRCameraController").rotation = Quaternion.Lerp(transform.Find("OVRCameraController").rotation, realRotation, 0.1f);
+			gameObject.GetComponent<PlayerInfo>().id = realId;
+			gameObject.GetComponent<PlayerInfo>().name = realName;
 		}
 	}
 	
@@ -28,6 +33,8 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 			// This is OUR player. We need to send our actual position to the network.
 			stream.SendNext(transform.position);
 			stream.SendNext(transform.Find("ForwardDirection").rotation);
+			stream.SendNext(gameObject.GetComponent<PlayerInfo>().id);
+			stream.SendNext(gameObject.GetComponent<PlayerInfo>().name);
 		}
 		else {
 			// This is someone else's player. We need to receive their position (as of a few
