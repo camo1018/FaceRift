c98f0673ab9b5f4b0437143aa37f322d2897d3ac
Paul Park <camo1018@yahoo.com>
2014-09-20 22:22:33 -0400

    Authentication loaded onto Unity

---
+++ unity/Assets/Integration/Authentication.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Authentication : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string[] arguments = Environment.GetCommandLineArgs();
+		
+		var argument = arguments[1];
+		
+		var authenticationToken = argument.Substring(8);
+		
+		Debug.Log("Auth Token " + authenticationToken);
+		
+		Toolbox.Instance.authenticationToken = authenticationToken;		
+	}
+}
